aa7546e64ab99196925e68e95850ac14523a2d0b
BINK <bink@email.ua>
2023-09-07 16:52:06 +0300

    add bg to main title

--- src/sections/Hero.jsx
+++ src/sections/Hero.jsx
@@ -17,10 +17,8 @@ const Hero = () => {
         <p className="font-montserrat text-xl text-coral-red">
           Our Summer Collection
         </p>
-        <h1 className="mt-10 font-palanquin text-8xl font-bold max-sm:text-[72px] max-sm:leading-[82px]">
-          <span className="relative z-10 pr-10 xl:whitespace-nowrap xl:bg-white">
-            The New Arrival
-          </span>
+        <h1 className="relative z-10 mt-10 pr-4 font-palanquin text-8xl font-bold max-sm:text-[72px] max-sm:leading-[82px] xl:whitespace-nowrap xl:bg-white">
+          <span className=" pr-10 ">The New Arrival</span>
           <br />
           <span className="mt-3 inline-block text-coral-red ">Nike</span>{" "}
           Collection
